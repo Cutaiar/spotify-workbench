b4421381163e44496fc42b63468d0b02a38df52c
Trenton Morrell <mtrent6@vt.edu>
2020-08-26 14:25:14 -0400

    adding code to get access token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,19 @@ import * as querystring from "query-string";
 import SpotifyWebApi from "spotify-web-api-js";
 import yoinkImage from "./yoink.jpg";
 import cover_b64 from "./cover.js"
+import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import hash from "./hash";
 
 const spotifyApi = new SpotifyWebApi();
 
 function App() {
+
+
+
+  ///NOTE I DONT THINK WE NEED ANY OF THE BELOW CODE IN TH BETWEEN THESE LINES
+  //-----------------------------------------------------------------------------------------------
+
+
   // Set up state to hold token for l8r and a bool for showing the final wallpaper
   const [token, setToken] = useState("fake UNSET token");
   const [showWallpaper, setShowWallpaper] = useState(false);
@@ -60,32 +69,41 @@ function App() {
 
   const _onGenerateClick = (props) => {
     console.log("Generate Clicked");
-
+    
     // Use spotifyApi to yoink all album objects
-
+    
     // extract image links from resposnse
-
+    
     // send image links in a separate object to our image-generation service
-
+    
     // get back the finished image
-
+    
     // update screen with downloadable/copyable image
     setShowWallpaper(true);
   };
 
-  return (
+
+  
+//-----------------------------------------------------------------------------------------------
+let token2 = hash.access_token;  
+return (
     <div className="App">
       <h1>Album Cover Wallpaper Generator</h1>
-      <button className="Button" onClick={_onAuthClick}>
+      {/* <button className="Button" onClick={_onAuthClick}>
         Connect To Spotify
-      </button>
-      <p>Token: {token}</p>
-      <button className="Button" onClick={_onGenerateClick}>
+      </button> */}
+      <a className="btn btn--loginApp-link"
+        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+          "%20"
+        )}&response_type=token&show_dialog=true`}>
+        Login to Spotify</a>
+      <p>Token: {token2}</p>
+      <button className="btn2 btn--loginApp-link" onClick={_onGenerateClick}>
         Generate Phone Wallpaper
       </button>
       <br></br><br></br>
       {showWallpaper && <img src={yoinkImage} height="100px" />}
-      {showWallpaper && <img src={cover_b64} height="100px"/>}
+      {showWallpaper && <img src={cover_b64} height="100px" />}
     </div>
   );
 }
