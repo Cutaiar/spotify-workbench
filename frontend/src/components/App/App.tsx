7ec5ac642e071f3bfcb6f2195d3d8cccc216f7db
Dillon M Cutaiar <cutaiar@Dillons-MacBook-Air-2.local>
2020-10-20 22:27:52 -0600

    Convert to functional, pull spot auth out to app, expose auth in childrens props

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { Component } from "react";
+import React from "react";
 import "./App.css";
 import {
   BrowserRouter as Router,
@@ -11,6 +10,18 @@ import {
 import { Button } from "primereact/button";
 import { RunPlaylist } from "../RunPlaylist/RunPlaylist";
 import { GenerateWallpaper } from "../GenerateWallpaper/GenerateWallpaper";
+import hash from "../../common/hash";
+import SpotifyWebApi from "spotify-web-api-js";
+import {
+  authEndpoint,
+  clientId,
+  redirectUri,
+  scopes,
+} from "../../common/config";
+
+const connectToSpotifyLink = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
 
 const Home = () => {
   return (
@@ -21,45 +32,73 @@ const Home = () => {
   );
 };
 
-const WallpaperRoute = () => {
-  return <GenerateWallpaper />;
-};
-
 const RunPlaylistRoute = () => {
   return <RunPlaylist />;
 };
 
-class App extends Component<any, any> {
-  render() {
-    return (
-      <Router>
-        <div>
-          <nav>
-            <NavLink className="navlink-style" to="/">
-              <Button style={{ textDecoration: "none" }}>Home</Button>
-            </NavLink>
-            <NavLink className="navlink-style" to="/wallpaper">
-              <Button>wallpaper</Button>
-            </NavLink>
-            <NavLink className="navlink-style" to="/runplaylist">
-              <Button>run playlist</Button>
-            </NavLink>
-          </nav>
-
-          <Switch>
-            <Route path="/wallpaper">
-              <WallpaperRoute />
-            </Route>
-            <Route path="/runplaylist">
-              <RunPlaylistRoute />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+export interface ISpotifyUser {
+  profileImage?: string;
+  token: string;
+  spotifyApi: any;
 }
+
+const App: React.FC = (props) => {
+  const [spotifyUser, setSpotifyUser] = React.useState<ISpotifyUser>(undefined);
+
+  const WallpaperRoute = () => {
+    return <GenerateWallpaper spotifyUser={spotifyUser} />;
+  };
+
+  React.useEffect(() => {
+    // Set token
+    let _token = (hash as any).access_token;
+    if (_token) {
+      const user: ISpotifyUser = {
+        profileImage: undefined,
+        token: _token,
+        spotifyApi: new SpotifyWebApi().setAccessToken(_token),
+      };
+      setSpotifyUser(user);
+    }
+  }, []);
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <NavLink className="navlink-style" to="/">
+            <Button style={{ textDecoration: "none" }}>Home</Button>
+          </NavLink>
+          <NavLink className="navlink-style" to="/wallpaper">
+            <Button>wallpaper</Button>
+          </NavLink>
+          <NavLink className="navlink-style" to="/runplaylist">
+            <Button>run playlist</Button>
+          </NavLink>
+        </nav>
+
+        <Button
+          onClick={() => {
+            window.location.href = connectToSpotifyLink;
+          }}
+          icon={spotifyUser?.token ? "pi pi-check" : "pi pi-sign-in"}
+          label={spotifyUser?.token ? "Token acquired" : "Connect To Spotify"}
+          className={`p-button-${spotifyUser?.token ? "success" : "info"}`}
+        ></Button>
+
+        <Switch>
+          <Route path="/wallpaper">
+            <WallpaperRoute />
+          </Route>
+          <Route path="/runplaylist">
+            <RunPlaylistRoute />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 export { App };
---
+++ frontend/src/components/GenerateWallpaper/GenerateWallpaper.tsx
@@ -0,0 +1,124 @@
+import React, { Component, useEffect, useState } from "react";
+import "./GenerateWallpaper.css";
+import axios from "axios";
+import { Button } from "primereact/button";
+import { ISpotifyUser } from "../App/App";
+
+interface IGenerateWallpaperProps {
+  spotifyUser: ISpotifyUser;
+}
+
+interface IGenerateWallpaperState {
+  listItems: any[];
+  showWallpaper: boolean;
+  wallpaperResponse: string;
+}
+const GenerateWallpaper: React.FC<IGenerateWallpaperProps> = (props) => {
+  const initialState: IGenerateWallpaperState = {
+    listItems: [],
+    showWallpaper: false,
+    wallpaperResponse: undefined,
+  };
+  const [state, setState] = React.useState<IGenerateWallpaperState>(
+    initialState
+  );
+
+  const onGenerateClick = () => {
+    let promises = [];
+    let responseLength = 0;
+    let imageURIs: any[] = [];
+
+    for (let i = responseLength; i < 10; i++) {
+      promises.push(
+        axios.get("https://api.spotify.com/v1/me/tracks", {
+          headers: { Authorization: "Bearer " + props.spotifyUser?.token },
+          params: {
+            offset: i * 50,
+            limit: 50,
+          },
+        })
+      );
+    }
+    axios
+      .all(promises)
+      .then(
+        axios.spread((...responses) => {
+          responses.forEach((response) => {
+            console.log(response);
+            response["data"]["items"].forEach((item: any) => {
+              if (item["track"]["album"]["images"][2]) {
+                imageURIs.push(item["track"]["album"]["images"][2]["url"]);
+              }
+            });
+          });
+          setState({
+            listItems: imageURIs,
+            showWallpaper: true,
+            wallpaperResponse: undefined,
+          });
+        })
+      )
+      .catch((errors) => {
+        console.error(errors);
+      });
+  };
+
+  const onUploadClicked = async () => {
+    // Get the URIs from state
+    const URIs = state.listItems;
+
+    // Hit the wp-service with the list of URIs
+    console.log("Sending URIs to server");
+    const resp = await fetch("http://localhost:3001/", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(URIs), // TODO [cutaiar] state.listItems may not have all the items we want depending on when you call this method since it only updates between renders
+    });
+
+    // Save the wallpaper from the response in state
+    const wallpaper = await resp.text();
+    console.log("Got Wallpaper");
+    setState({ ...state, wallpaperResponse: wallpaper });
+  };
+
+  return (
+    <div className="GenerateWallpaperRoot">
+      <h1>Album Cover Wallpaper Generator</h1>
+
+      <Button
+        className="p-button-primary"
+        onClick={onGenerateClick}
+        disabled={!props.spotifyUser?.token}
+        icon="pi pi-images"
+        label="Get Images from Spotify"
+      ></Button>
+      <Button
+        className="p-button-primary"
+        onClick={onUploadClicked}
+        disabled={!state.showWallpaper}
+        icon="pi pi-play"
+        label="Generate Wallpaper (backend)"
+      ></Button>
+
+      {state.wallpaperResponse && (
+        <>
+          <p>Wallpaper from backend: click to download</p>
+          <a href={state.wallpaperResponse} download>
+            <img src={state.wallpaperResponse} height="500"></img>
+          </a>
+        </>
+      )}
+
+      {state.showWallpaper && (
+        <>
+          <p>Images from Spotify:</p>
+          {state.listItems.map((item, i) => {
+            return <img src={item} height="50px" key={i} />;
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export { GenerateWallpaper };
