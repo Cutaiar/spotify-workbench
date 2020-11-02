c06716fb11386578fa87bca37492ac8b6343281f
Dillon M Cutaiar <cutaiar@Dillons-MacBook-Air-2.local>
2020-11-01 21:53:51 -0700

    Added range for number of songs to fetch Added RunPlaylist.css for style

--- frontend/src/components/RunPlaylist/RunPlaylist.tsx
+++ frontend/src/components/RunPlaylist/RunPlaylist.tsx
@@ -5,6 +5,9 @@ import SpotifyWebApi from "spotify-web-api-js";
 import { getImageForSpotifyTracks } from "../../common/util";
 import { ISpotifyUser } from "../App/App";
 import "primeflex/primeflex.css";
+import { Slider } from "primereact/slider";
+import { InputText } from "primereact/inputtext";
+import "./RunPlaylist.css";
 
 interface IRunPlaylistProps {
   spotifyUser: ISpotifyUser;
@@ -13,8 +16,12 @@ interface IRunPlaylistProps {
 export const RunPlaylist: React.FunctionComponent<IRunPlaylistProps> = (
   props
 ) => {
+  const [numberOfSongsToFetch, setNumberOfSongsToFetch] = React.useState(10);
+
   const getRecentSongs = async () => {
-    const options: SpotifyApi.RecentlyPlayedParameterObject = { limit: 10 };
+    const options: SpotifyApi.RecentlyPlayedParameterObject = {
+      limit: numberOfSongsToFetch,
+    };
     const songs = await props.spotifyUser.spotifyApi.getMyRecentlyPlayedTracks(
       options
     );
@@ -70,10 +77,42 @@ export const RunPlaylist: React.FunctionComponent<IRunPlaylistProps> = (
     );
   };
 
+  const fetchLimit = 50;
+  const isInputValid = () => {
+    return numberOfSongsToFetch < fetchLimit + 1 && numberOfSongsToFetch > 0;
+  };
+
   return (
-    <>
+    <div className={"RunPlaylistRoot"}>
       <h1> Run Playlists</h1>
-      <Button onClick={() => getRecentSongs()} disabled={!props.spotifyUser}>
+      <div style={{ width: "200px" }}>
+        <InputText
+          style={{ width: "100%" }}
+          value={numberOfSongsToFetch}
+          keyfilter={/^[0-5]?[0-9]$/}
+          className={isInputValid() ? "" : "p-invalid"}
+          onChange={(e) => {
+            let target = e.target as HTMLInputElement;
+            setNumberOfSongsToFetch(+target.value);
+          }}
+        />
+        <Slider
+          min={1}
+          max={50}
+          value={numberOfSongsToFetch}
+          onChange={(e) => {
+            setNumberOfSongsToFetch(e.value as number);
+          }}
+        />
+        {!isInputValid() && (
+          <small className="p-invalid"> {`Between 0 and ${fetchLimit}`}</small>
+        )}
+      </div>
+
+      <Button
+        onClick={() => getRecentSongs()}
+        disabled={!props.spotifyUser || !isInputValid()}
+      >
         Get Recent Songs
       </Button>
       <Button
@@ -98,6 +137,6 @@ export const RunPlaylist: React.FunctionComponent<IRunPlaylistProps> = (
           />
         </div>
       </div>
-    </>
+    </div>
   );
 };
