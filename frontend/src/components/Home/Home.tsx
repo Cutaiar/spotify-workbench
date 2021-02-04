952d31a92df3558ba5010beb1b305e674ce359cf
Dillon Cutaiar <dillonc@vt.edu>
2021-02-04 00:57:27 -0700

    update home look

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -3,19 +3,22 @@ import "primeflex/primeflex.css";
 
 export const Home: React.FC = (props) => {
   return (
-    <div
-      className={"p-d-flex p-jc-center p-flex-column p-ai-center"}
-      style={{ height: "100%" }}
-    >
-      <div style={{ width: 300, color: "white" }}>
-        <h3 style={{ color: "white" }}>Welcome to Spotify Workbench</h3>
-        <p>
-          Welcome to your Spotify workbench. This is a collection of tools,
-          addons, and nifty functionality which aims to add both depth and
-          simplicity to your modern music exploration and consumption. Right
-          now, you can create a wallpaper and make a playlist out of your
-          recently listened songs. Connect to Spotify (top right) to get
-          started!
+    <div style={{ height: "100%" }}>
+      <div style={{ width: 1000 }}>
+        <p
+          style={{
+            paddingLeft: "50px",
+            paddingTop: "50px",
+            color: "#4E565C",
+            fontSize: 25,
+          }}
+        >
+          Welcome to your Spotify workbench.
+          <br />
+          <br />
+          This is a collection of tools, addons, and nifty functionality which
+          aims to add both depth and simplicity to your modern music exploration
+          and consumption.
         </p>
       </div>
     </div>
