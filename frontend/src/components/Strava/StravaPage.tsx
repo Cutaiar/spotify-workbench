d0fdf2d33f0595f13abd8c3dc57a7a8f90ebbb09
Dillon Cutaiar <dillonc@vt.edu>
2022-04-15 09:43:45 -0400

    make map dark

--- frontend/src/components/Strava/StravaPage.tsx
+++ frontend/src/components/Strava/StravaPage.tsx
@@ -98,8 +98,8 @@ export const StravaPage: React.FC = () => {
     return (
       <MapContainer center={center} zoom={13} style={{ height: "100%" }}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
         />
         {polyline && <Polyline positions={polyline} />}
       </MapContainer>
